55df5af23c9c74ec1b1d99e5f3bf6585aa50f5b4
py-dev-algebra <python.developer.edukacija@gmail.com>
2023-12-05 20:17:10 +0100

    Added len function

---
+++ lists_05.py
@@ -0,0 +1,14 @@
+numbers = []
+
+for i in range(1, 101):
+    numbers.append(i)
+
+
+for number in numbers:
+    print(number, end=' ')
+
+print()
+
+number_of_list_elements = len(numbers)
+print(f'Lista "numbers" ima: {number_of_list_elements} elemenata.')
+print(f'Lista "numbers" ima: {len(numbers)} elemenata.')
---
+++ zadatak_02.py
@@ -0,0 +1,4 @@
+# Napisite program koji zbraja brojeve od 1 do nekog broja.
+# Neka korisnik definira do kojeg broja zeli ce biti zbrajanje
+# Ispiste zboj te ispiste srednju vrijednost (zbroj / broj elemenata)
+
