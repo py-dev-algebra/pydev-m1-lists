5e0281a16749dfde216cb44d33fe4398fb245c3d
py-dev-algebra <python.developer.edukacija@gmail.com>
2023-12-05 20:53:06 +0100

    Zadaca

--- lists_05.py
+++ lists_05.py
@@ -12,3 +12,10 @@ print()
 number_of_list_elements = len(numbers)
 print(f'Lista "numbers" ima: {number_of_list_elements} elemenata.')
 print(f'Lista "numbers" ima: {len(numbers)} elemenata.')
+
+
+numbers[49] = 'Pedeset'
+
+for number in numbers:
+    print(number, end=' ')
+
---
+++ zadatak_03.py
@@ -0,0 +1,6 @@
+# Koliko slova ima u rijeci programiranje?
+text = 'Programiranje'
+
+print(f'Broj slova u rijeci "Programiranje" je {len(text)}')
+
+# Zbroj slova u rijeci Programiranje
