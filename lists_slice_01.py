424588fdc22b2c6701006dcf4b9c0cbc009c8c71
py-dev-algebra <python.developer.edukacija@gmail.com>
2023-12-05 21:06:52 +0100

    Create lists_slice_01.py

---
+++ lists_slice_01.py
@@ -0,0 +1,12 @@
+numbers = []
+
+for i in range(100, 201):
+    numbers.append(i)
+
+
+print(numbers)
+print()
+
+# SLICE liste je naziv_liste[START : KRAJ : KORAK]
+numbers_sliced = numbers[0 : 20 : 3]
+print(numbers_sliced)
