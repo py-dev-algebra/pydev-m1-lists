7a5305e21bfd11f5c7ac55277b9ff0a41576b92e
py-dev-algebra <python.developer.edukacija@gmail.com>
2023-12-07 19:35:57 +0100

    Update lists_slice_02.py

--- lists_slice_02.py
+++ lists_slice_02.py
@@ -1,8 +1,30 @@
 numbers = []
-print(numbers)
-print()
 
-for number in range(1, 101):
+for number in range(100, 201):
     numbers.append(number)
-print(numbers)
-print()
+
+#                           S    F   Step
+# selected_numbers = numbers[20 : 45 : 10] # -> od indeksa 20 do indeksa 45 dvaki deseti
+# print(selected_numbers)
+
+# Sve elemente od prvog do 45-og
+# selected_numbers = numbers[0 : 45 : 2]
+# selected_numbers = numbers[ : 45 : 2]
+# print(selected_numbers)
+
+
+# selected_numbers = numbers[45 : 0 : -2]
+# print(selected_numbers)
+
+# last_element = numbers[-1]
+# before_last_element = numbers[-2]
+
+# selected_numbers = numbers[-1 : : -1]
+# print(selected_numbers)
+
+# selected_numbers = numbers[ : : -1]
+# print(selected_numbers)
+
+text = 'Programiranje'
+obrnuto = text[ : : -1]
+print(obrnuto)
